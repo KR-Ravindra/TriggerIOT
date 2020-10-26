7e42e923ab73c9f1dce4e6444591d4557a9e7094
Kathi Raja Ravindra <42912207+KR-Ravindra@users.noreply.github.com>
2020-10-26 12:08:59 +0530

    Create sh

---
+++ create.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt install uuid-runtime
+jobID=$(uuidgen)
+
+aws configure set default.region us-east-1
+aws iot create-job  \
+      --job-id $jobID  \
+      --targets arn:aws:iot:us-east-1:629707839273:thing/UV-ARDEN-001 \
+      --document-source https://s3.amazonaws.com/ota-addverb/download-files.json  \
+      --timeout-config inProgressTimeoutInMinutes=100 \
+      --job-executions-rollout-config "{ \"exponentialRate\": { \"baseRatePerMinute\": 50, \"incrementFactor\": 2, \"rateIncreaseCriteria\": { \"numberOfSucceededThings\": 1000}}, \"maximumPerMinute\": 1000}" \
+      --abort-config "{ \"criteriaList\": [ { \"action\": \"CANCEL\", \"failureType\": \"FAILED\", \"minNumberOfExecutedThings\": 100, \"thresholdPercentage\": 20}, { \"action\": \"CANCEL\", \"failureType\": \"TIMED_OUT\", \"minNumberOfExecutedThings\": 200, \"thresholdPercentage\": 50}]}" 
