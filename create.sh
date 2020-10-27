ebdf736e7beede09d19720832a9d65f977ea9358
Kathi Raja Ravindra <42912207+KR-Ravindra@users.noreply.github.com>
2020-10-27 12:11:05 +0530

    Demo fasak

--- create.sh
+++ create.sh
@@ -3,13 +3,14 @@
 apt update
 apt install uuid-runtime -y
 jobID=$(uuidgen)
+fileName=download-files-demo.json
 echo $jobID
 aws configure set default.region us-east-1
 aws iot list-jobs
 aws iot create-job  \
       --job-id $jobID  \
       --targets arn:aws:iot:us-east-1:524057922460:thing/UV-Arden-001 \
-      --document-source https://s3.amazonaws.com/addverb-firmware/download-files.json  \
+      --document-source https://s3.amazonaws.com/addverb-firmware/$fileName  \
       --timeout-config inProgressTimeoutInMinutes=100 \
       --job-executions-rollout-config "{ \"exponentialRate\": { \"baseRatePerMinute\": 50, \"incrementFactor\": 2, \"rateIncreaseCriteria\": { \"numberOfSucceededThings\": 1000}}, \"maximumPerMinute\": 1000}" \
       --abort-config "{ \"criteriaList\": [ { \"action\": \"CANCEL\", \"failureType\": \"FAILED\", \"minNumberOfExecutedThings\": 100, \"thresholdPercentage\": 20}, { \"action\": \"CANCEL\", \"failureType\": \"TIMED_OUT\", \"minNumberOfExecutedThings\": 200, \"thresholdPercentage\": 50}]}" 
