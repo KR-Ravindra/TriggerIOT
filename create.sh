516403e8ae5a6569689c015b668a0844826df0d1
Kathi Raja Ravindra <42912207+KR-Ravindra@users.noreply.github.com>
2020-10-27 11:33:44 +0530

    Finished

--- create.sh
+++ create.sh
@@ -9,7 +9,7 @@ aws iot list-jobs
 aws iot create-job  \
       --job-id $jobID  \
       --targets arn:aws:iot:us-east-1:524057922460:thing/UV-Arden-001 \
-      --document-source https://s3.amazonaws.com/addverb-firmware/UV-Arden-001  \
+      --document-source https://s3.amazonaws.com/addverb-firmware/UV-Arden-001/download-files.json  \
       --timeout-config inProgressTimeoutInMinutes=100 \
       --job-executions-rollout-config "{ \"exponentialRate\": { \"baseRatePerMinute\": 50, \"incrementFactor\": 2, \"rateIncreaseCriteria\": { \"numberOfSucceededThings\": 1000}}, \"maximumPerMinute\": 1000}" \
       --abort-config "{ \"criteriaList\": [ { \"action\": \"CANCEL\", \"failureType\": \"FAILED\", \"minNumberOfExecutedThings\": 100, \"thresholdPercentage\": 20}, { \"action\": \"CANCEL\", \"failureType\": \"TIMED_OUT\", \"minNumberOfExecutedThings\": 200, \"thresholdPercentage\": 50}]}" 
